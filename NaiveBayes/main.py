d792892ba83567210743fa816a405471ed11805b
Aimeê Miranda <aimeemiranribe@gmail.com>
2022-07-01 10:45:01 -0300

    suavização

--- NaiveBayes/main.py
+++ NaiveBayes/main.py
@@ -6,27 +6,16 @@ from datas import matrices as mt
 nome_arquivo = "NaiveBayes/datas/datase01.csv"
 matriz_treinamento, matriz_teste = dp.construcao(nome_arquivo)
 classe="seguro"
-#print(len(matriz_teste[next(iter(matriz_teste))]))
 
 for i in range(len(matriz_teste[next(iter(matriz_teste))])):
+
     aux, Y = nb.vetorinf(matriz_teste, i)
 
-    #cont_ocY, prob_aposteriori, auxY, prob_aprioriY = 
-    #cont_colunas, total=nb.probs(matriz_treinamento , aux, Y,classe)
-    #maxkey = nb.naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY)
-    #dp.insercao(maxkey, aux, Y)
+    maxkey= nb.naive(matriz_treinamento, Y, aux, nb.ctclasse)
+    print(classe)
 
-    nb.naive(matriz_treinamento, Y, aux, nb.ctclasse)
-
-
-
-    """print(("#"*40),"\n\nDadas as informações:\n",)
+    print("\n\n",("#"*40),"\n\nDadas as informações:\n",)
     for i in aux.keys():
         if(i != Y):
             print(i,": ",aux[i])
-    print("\nCom as probabilidades aposteriories:\n")
-    for i in prob_aposteriori.keys():
-        print(i,": ",prob_aposteriori[i])
-
     print("\nA predição para o resultado de ",Y," é: ",maxkey,"\n")
-    """
--- NaiveBayes/naive_bayes.py
+++ NaiveBayes/naive_bayes.py
@@ -65,7 +65,6 @@ def naive(matriz_treinamento, Y, aux, ctclasse):
     for i in matriz_treinamento[Y]:
         classe[i]=0
     classe1 = classe.copy()
-    #print(len(classe))
     for i in classe1:
         cont_classe = ctclasse(matriz_treinamento, Y, i)
         x = cont_classe[Y]/len(matriz_treinamento[Y])
@@ -73,75 +72,10 @@ def naive(matriz_treinamento, Y, aux, ctclasse):
 
     for i in classe.keys():
         classe[i] = round(classe1[max(classe1)] + probs(matriz_treinamento , aux, Y, i, ctclasse),4)      
-        #print(log(x,10).real)    
-    #print(classe['seguro']>classe['perigoso'])   
 
     max_value = None
     for num in classe.keys():
         if (max_value is None or classe[num] > max_value):
             max_value = classe[num]
             maxkey = num
-    print(maxkey)
-
-    #print(cont_ocorrencias)
-    #print(V)
-    """       
-    prob_aprioriX = cont_ocorrencias.copy()
-    
-    for i in cont_ocorrencias.keys():
-        prob_aprioriX[i]=prob_aprioriX[i]/(len(matriz_treinamento[Y])+1)
-    
-    #dos y
-    for i in matriz_treinamento[Y]:
-        if i in cont_ocY:
-            cont_ocY[i] += 1
-        else:
-            cont_ocY[i] = 1
-    prob_aprioriY = cont_ocY.copy()
-    for i in prob_aprioriY.keys():
-        prob_aprioriY[i]=prob_aprioriY[i]/len(matriz_treinamento[Y])
-    
-    #prob da interseção
-    auxY = []
-    auxa = []
-    for i in aux.keys():
-        if(i != Y):
-            for j in range(len(matriz_treinamento[Y])):  
-                #print(matriz_treinamento[i][j] == aux[i],"\n")
-                #print(matriz_treinamento[i][j],"\n")
-                #print(aux[i],"\n\n")              
-                if (matriz_treinamento[i][j] == aux[i]):
-                    auxx = matriz_treinamento[i][j]+"|"+matriz_treinamento[Y][j]
-                    if auxx in cont_intersecao.keys():
-                        cont_intersecao[auxx]+=1
-                    else: 
-                        auxY.append(matriz_treinamento[Y][j])
-                        auxa.append(matriz_treinamento[i][j])
-                        cont_intersecao[auxx]=1
-                    
-    prob_aposteriori = cont_intersecao.copy()
-    auxXY = list(prob_aposteriori.keys())
-
-    for i in range(len(prob_aposteriori)):
-        #print(cont_ocY[auxY[i]])
-        prob_aposteriori[auxXY[i]]=prob_aprioriX[auxa[i]]*(prob_aposteriori[auxXY[i]]/cont_ocY[auxY[i]])
-    return cont_ocY, prob_aposteriori, auxY, prob_aprioriY"""
-
-"""def naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY):
-    #6. o naive bayes de fato
-    import cmath 
-    aux = {}
-    keysap = list(prob_aposteriori.keys())
-    prob_naiveY={}
-    for i in cont_ocY.keys():
-        #somatorio:
-        for j in range(len(prob_aposteriori.keys())):
-            if auxY[j] in aux.keys():
-                aux[auxY[j]]= aux[auxY[j]] + log(prob_aposteriori[keysap[j]],10)
-            else:
-                aux[auxY[j]]= log(prob_aposteriori[keysap[j]],10)
-        prob_naiveY[i]= log(prob_aprioriY[i],10) + aux[i]
-        prob_naiveY[i] = prob_naiveY[i].real
-    
-    maxkey=max(prob_naiveY, key=prob_naiveY.get)
-    return maxkey"""
+    return maxkey

836c66912e3615d17f562ee2d71fe733c870e62a
Aimeê Miranda <aimeemiranribe@gmail.com>
2022-07-01 10:27:54 -0300

    suavização

--- NaiveBayes/main.py
+++ NaiveBayes/main.py
@@ -5,22 +5,28 @@ from datas import matrices as mt
 #fornecendo o nome do arquivo:
 nome_arquivo = "NaiveBayes/datas/datase01.csv"
 matriz_treinamento, matriz_teste = dp.construcao(nome_arquivo)
-
+classe="seguro"
 #print(len(matriz_teste[next(iter(matriz_teste))]))
 
 for i in range(len(matriz_teste[next(iter(matriz_teste))])):
     aux, Y = nb.vetorinf(matriz_teste, i)
-    
-    cont_ocY, prob_aposteriori, auxY, prob_aprioriY = nb.probs(matriz_treinamento , aux, Y)
-    maxkey = nb.naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY)
-    dp.insercao(maxkey, aux, Y)
 
-    print(("#"*40),"\n\nDadas as informações:\n",)
+    #cont_ocY, prob_aposteriori, auxY, prob_aprioriY = 
+    #cont_colunas, total=nb.probs(matriz_treinamento , aux, Y,classe)
+    #maxkey = nb.naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY)
+    #dp.insercao(maxkey, aux, Y)
+
+    nb.naive(matriz_treinamento, Y, aux, nb.ctclasse)
+
+
+
+    """print(("#"*40),"\n\nDadas as informações:\n",)
     for i in aux.keys():
         if(i != Y):
             print(i,": ",aux[i])
     print("\nCom as probabilidades aposteriories:\n")
     for i in prob_aposteriori.keys():
         print(i,": ",prob_aposteriori[i])
-    
+
     print("\nA predição para o resultado de ",Y," é: ",maxkey,"\n")
+    """
--- NaiveBayes/naive_bayes.py
+++ NaiveBayes/naive_bayes.py
@@ -1,6 +1,6 @@
 
 from cmath import log
-
+import cmath
 
 def vetorinf(matriz_teste, KEY):
     """4. vetor que será utilizado, KEY é a linha que será analisada"""
@@ -14,30 +14,89 @@ def vetorinf(matriz_teste, KEY):
 
     return aux, Y
 
-def probs(matriz_treinamento , aux, Y):
+def ctclasse(matriz_treinamento, Y, classe):
+    cont_classe={}
+    for i in matriz_treinamento[Y]:
+        if i == classe:
+            if Y in cont_classe:
+                cont_classe[Y] += 1
+            else:
+                cont_classe[Y] = 1
+    return cont_classe
+
+def probs(matriz_treinamento , aux, Y, classe, ctclasse):
     """5. construção das probabilidades /precisa de uma limpeza de cósigo"""
     cont_ocorrencias = {}
-    cont_ocY = {}
-    cont_intersecao={}
+    cont_colunas={}
 
+    
     #dos valores
-    auxkeys = [*aux.keys()]
+    auxkeys = [*aux]
     for j in range(len(aux)):
         for i in matriz_treinamento[auxkeys[j]]:
-            if aux[auxkeys[j]] == i:
-                if aux[auxkeys[j]] in cont_ocorrencias: 
-                    cont_ocorrencias[aux[auxkeys[j]]] +=1
-                else:
-                    cont_ocorrencias[aux[auxkeys[j]]] = 1
-    prob_aprioriX = cont_ocorrencias.copy()
+            if auxkeys[j] != Y:
+                cont_ocorrencias[aux[auxkeys[j]]] = 1
+    for j in range(len(cont_ocorrencias)):
+        cont=0
+        for i in matriz_treinamento[auxkeys[j]]:
+            if aux[auxkeys[j]] == i and matriz_treinamento[Y][cont]==classe:
+                cont_ocorrencias[aux[auxkeys[j]]] +=1
+            cont+=1
+    
+    cont_classe=ctclasse(matriz_treinamento, Y, classe)
+
+    V= len(cont_ocorrencias)
+
+    #naive:
+    total = 0.0
     for i in cont_ocorrencias.keys():
-        prob_aprioriX[i]=prob_aprioriX[i]/len(matriz_treinamento[Y])
+        
+        x= cont_ocorrencias[i]/(V+cont_classe[Y])
+        cont_colunas[i]=log(x,10)
+        total+= cont_colunas[i]
+    return total.real   
+    
+
+
+
+def naive(matriz_treinamento, Y, aux, ctclasse):
+    #contagem de classes:
+    classe={}
+    for i in matriz_treinamento[Y]:
+        classe[i]=0
+    classe1 = classe.copy()
+    #print(len(classe))
+    for i in classe1:
+        cont_classe = ctclasse(matriz_treinamento, Y, i)
+        x = cont_classe[Y]/len(matriz_treinamento[Y])
+        classe1[i]=log(x,10).real 
+
+    for i in classe.keys():
+        classe[i] = round(classe1[max(classe1)] + probs(matriz_treinamento , aux, Y, i, ctclasse),4)      
+        #print(log(x,10).real)    
+    #print(classe['seguro']>classe['perigoso'])   
+
+    max_value = None
+    for num in classe.keys():
+        if (max_value is None or classe[num] > max_value):
+            max_value = classe[num]
+            maxkey = num
+    print(maxkey)
+
+    #print(cont_ocorrencias)
+    #print(V)
+    """       
+    prob_aprioriX = cont_ocorrencias.copy()
+    
+    for i in cont_ocorrencias.keys():
+        prob_aprioriX[i]=prob_aprioriX[i]/(len(matriz_treinamento[Y])+1)
+    
     #dos y
     for i in matriz_treinamento[Y]:
         if i in cont_ocY:
-            cont_ocY[i] += 1.0
+            cont_ocY[i] += 1
         else:
-            cont_ocY[i] = 1.0
+            cont_ocY[i] = 1
     prob_aprioriY = cont_ocY.copy()
     for i in prob_aprioriY.keys():
         prob_aprioriY[i]=prob_aprioriY[i]/len(matriz_treinamento[Y])
@@ -48,9 +107,9 @@ def probs(matriz_treinamento , aux, Y):
     for i in aux.keys():
         if(i != Y):
             for j in range(len(matriz_treinamento[Y])):  
-                """print(matriz_treinamento[i][j] == aux[i],"\n")
-                print(matriz_treinamento[i][j],"\n")
-                print(aux[i],"\n\n") """               
+                #print(matriz_treinamento[i][j] == aux[i],"\n")
+                #print(matriz_treinamento[i][j],"\n")
+                #print(aux[i],"\n\n")              
                 if (matriz_treinamento[i][j] == aux[i]):
                     auxx = matriz_treinamento[i][j]+"|"+matriz_treinamento[Y][j]
                     if auxx in cont_intersecao.keys():
@@ -66,11 +125,10 @@ def probs(matriz_treinamento , aux, Y):
     for i in range(len(prob_aposteriori)):
         #print(cont_ocY[auxY[i]])
         prob_aposteriori[auxXY[i]]=prob_aprioriX[auxa[i]]*(prob_aposteriori[auxXY[i]]/cont_ocY[auxY[i]])
-    
-    return cont_ocY, prob_aposteriori, auxY, prob_aprioriY
+    return cont_ocY, prob_aposteriori, auxY, prob_aprioriY"""
 
-def naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY):
-    """6. o naive bayes de fato"""
+"""def naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY):
+    #6. o naive bayes de fato
     import cmath 
     aux = {}
     keysap = list(prob_aposteriori.keys())
@@ -86,5 +144,4 @@ def naivebayes(cont_ocY, prob_aposteriori, auxY, prob_aprioriY):
         prob_naiveY[i] = prob_naiveY[i].real
     
     maxkey=max(prob_naiveY, key=prob_naiveY.get)
-    return maxkey
-
+    return maxkey"""
